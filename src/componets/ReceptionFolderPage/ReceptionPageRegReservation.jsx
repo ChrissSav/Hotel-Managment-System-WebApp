d189b29e0a5b44042e5e74a325d7d821b0d6898c
ChrissSav <dai17023@uom.edu.gr>
2020-03-04 19:39:26 +0200

    Add handleChangeInput in ReceptionPageRegReservation

--- src/componets/ReceptionFolderPage/ReceptionPageRegReservation.jsx
+++ src/componets/ReceptionFolderPage/ReceptionPageRegReservation.jsx
@@ -1,7 +1,52 @@
 import React, { Component } from "react";
 import "./ReceptionPageRegReservationStyle.css";
+
 class ReceptionPageRegReservation extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      //info
+      book_id: "",
+      employee_id: "",
+      book_date: "",
+      room_id: "",
+      //booking
+      costumer_id: "",
+      arrival: "",
+      departure: "",
+      diet: "",
+      num_of_adults: "",
+      num_of_minors: "",
+      parking: "",
+      //cost
+      room_cost: "",
+      tax: "",
+      cost_of_benefits: "",
+      cost_total: "",
+
+      //for room
+      wifi: "",
+      pool: "",
+      a_c: ""
+    };
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+  }
+
+  handleChangeInput(event) {
+    // console.log(event.target, event.target.value);
+    //console.log(event.target.type);
+    if (event.target.type == "checkbox") {
+      //console.log("1");
+      this.setState({
+        [event.target.id]: event.target.checked ? "yes" : "no"
+      });
+    } else {
+      //console.log("2");
+      this.setState({
+        [event.target.id]: event.target.value
+      });
+    }
+  }
   render() {
     return (
       <div className="ReceptionRe_container">
@@ -13,13 +58,25 @@ class ReceptionPageRegReservation extends Component {
                 <label>Κωδικός κράτησης</label>
               </th>
               <th align="right">
-                <input type="text" readOnly />
+                <input
+                  id="book_id"
+                  type="text"
+                  value={this.state.book_id}
+                  // onChange={this.handleChangeInput}
+                  readOnly
+                />
               </th>
               <th align="left">
                 <label>Κωδικός Γραμματέα</label>
               </th>
               <th align="right">
-                <input type="text" readOnly />
+                <input
+                  id="employee_id"
+                  type="text"
+                  value={this.state.employee_id}
+                  //  onChange={this.handleChangeInput}
+                  readOnly
+                />
               </th>
             </tr>
             <tr>
@@ -27,7 +84,13 @@ class ReceptionPageRegReservation extends Component {
                 <label>Ημερ/νια κράτησης</label>
               </th>
               <th align="right">
-                <input type="text" readOnly />
+                <input
+                  id="book_date"
+                  type="text"
+                  value={this.state.book_date}
+                  //  onChange={this.handleChangeInput}
+                  readOnly
+                />
               </th>
               <th align="left">
                 <label></label>
@@ -45,13 +108,21 @@ class ReceptionPageRegReservation extends Component {
                 <label>Άφιξη</label>
               </th>
               <th align="right">
-                <input type="date" />
+                <input
+                  id="arrival"
+                  type="date"
+                  onChange={this.handleChangeInput}
+                />
               </th>
               <th align="left">
                 <label>Διατροφή</label>
               </th>
               <th align="right">
-                <select className="diet">
+                <select
+                  id="diet"
+                  className="diet"
+                  onChange={this.handleChangeInput}
+                >
                   <option value="Χωρίς διατροφή">Χωρίς διατροφή</option>
                   <option value="Μόνο πρωινό">Μόνο πρωινό</option>
                   <option value="Ημιδιατροφή">Ημιδιατροφή</option>
@@ -64,16 +135,30 @@ class ReceptionPageRegReservation extends Component {
                 <label>Αναχώρηση</label>
               </th>
               <th align="right">
-                <input type="date" />
+                <input
+                  id="departure"
+                  type="date"
+                  onChange={this.handleChangeInput}
+                />
               </th>
               <th align="left">
                 <label className="check">
-                  <input type="checkbox" /> Κλιματισμός
+                  <input
+                    id="a_c"
+                    type="checkbox"
+                    onChange={this.handleChangeInput}
+                  />
+                  Κλιματισμός
                 </label>
               </th>
               <th align="right">
                 <label className="check" id="cc1">
-                  <input type="checkbox" /> Παροχή Wifi
+                  <input
+                    id="wifi"
+                    type="checkbox"
+                    onChange={this.handleChangeInput}
+                  />{" "}
+                  Παροχή Wifi
                 </label>
               </th>
             </tr>
@@ -82,17 +167,32 @@ class ReceptionPageRegReservation extends Component {
                 <label>Αρ. Ενηλίκων</label>
               </th>
               <th align="right">
-                <input type="number" min="0" />
+                <input
+                  id="num_of_adults"
+                  type="number"
+                  min="0"
+                  onChange={this.handleChangeInput}
+                  value={this.state.num_of_adults}
+                />
               </th>
               <th align="left">
                 <label className="check">
-                  <input type="checkbox" name="checkbox" value="pool" /> Πισίνα
+                  <input
+                    id="pool"
+                    type="checkbox"
+                    onChange={this.handleChangeInput}
+                  />
+                  Πισίνα
                 </label>
               </th>
               <th align="right">
                 <label className="check" id="cc2">
-                  <input type="checkbox" name="parking" value="parking" /> Θέση
-                  στάθμεσης
+                  <input
+                    id="parking"
+                    type="checkbox"
+                    onChange={this.handleChangeInput}
+                  />
+                  Θέση στάθμεσης
                 </label>
               </th>
             </tr>
@@ -101,17 +201,23 @@ class ReceptionPageRegReservation extends Component {
                 <label>Αρ. Ανηλίκων</label>
               </th>
               <th align="right">
-                <input type="number" min="0" />
+                <input
+                  id="num_of_minors"
+                  type="number"
+                  min="0"
+                  onChange={this.handleChangeInput}
+                  value={this.state.num_of_minors}
+                />
               </th>
               <th align="left">
                 <label>Τύπος δωματίου</label>
               </th>
               <th align="right">
-                <select>
-                  <option value="Single">Μονόκλινο</option>
-                  <option value="Double">Δίκλινο</option>
-                  <option value="Triple">Τρίκλινο</option>
-                  <option value="Family">Οικογενειακό</option>
+                <select id="room_type" onChange={this.handleChangeInput}>
+                  <option value="Μονόκλινο">Μονόκλινο</option>
+                  <option value="Δίκλινο">Δίκλινο</option>
+                  <option value="Τρίκλινο">Τρίκλινο</option>
+                  <option value="Οικογενειακό">Οικογενειακό</option>
                 </select>
               </th>
             </tr>
@@ -120,13 +226,19 @@ class ReceptionPageRegReservation extends Component {
                 <label>Κωδικός Πελάτη</label>
               </th>
               <th align="right">
-                <input type="text" readOnly />
+                <input
+                  id="costumer_id"
+                  type="text"
+                  readOnly
+                  onChange={this.handleChangeInput}
+                  value={this.state.costumer_id}
+                />
               </th>
               <th align="left">
                 <label>Αριθ. Δωματίου</label>
               </th>
               <th align="right" className="wrap_room">
-                <input type="text" readOnly className="makis" />
+                <input type="room_id" value={this.state.room_id} readOnly />
                 <button className="Search_room"></button>
               </th>
             </tr>
@@ -139,13 +251,25 @@ class ReceptionPageRegReservation extends Component {
                 <label>Κόστος δωματίου</label>
               </th>
               <th align="right">
-                <input type="number" readOnly />
+                <input
+                  id="room_cost"
+                  type="number"
+                  onChange={this.handleChangeInput}
+                  value={this.state.room_cost}
+                  readOnly
+                />
               </th>
               <th align="left">
                 <label>Φ.Π.Α.</label>
               </th>
               <th align="right">
-                <input type="number" readOnly />
+                <input
+                  id="tax"
+                  type="number"
+                  onChange={this.handleChangeInput}
+                  value={this.state.tax}
+                  readOnly
+                />
               </th>
             </tr>
             <tr>
@@ -153,18 +277,37 @@ class ReceptionPageRegReservation extends Component {
                 <label>Κόστος παροχών</label>
               </th>
               <th align="right">
-                <input type="number" readOnly />
+                <input
+                  id="cost_of_benefits"
+                  type="number"
+                  onChange={this.handleChangeInput}
+                  value={this.state.cost_of_benefits}
+                  readOnly
+                />
               </th>
               <th align="left">
                 <label>Τελικό Κόστος</label>
               </th>
               <th align="right">
-                <input type="number" readOnly />
+                <input
+                  id="cost_total"
+                  type="number"
+                  value={this.state.cost_total}
+                  onChange={this.handleChangeInput}
+                  readOnly
+                />
               </th>
             </tr>
           </tbody>
         </table>
-        <button className="btnregBook">Καταχώρηση</button>
+        <button
+          className="btnregBook"
+          onClick={() => {
+            console.log(this.state);
+          }}
+        >
+          Καταχώρηση
+        </button>
       </div>
     );
   }
